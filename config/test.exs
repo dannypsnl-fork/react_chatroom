85b852989b1fc24a33435f8b137ace493faf719a
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-07-16 03:02:00 +0800

    ci(add service postgres)

--- config/test.exs
+++ config/test.exs
@@ -12,6 +12,12 @@ config :react_chatroom, ReactChatroom.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
+if System.get_env("GITHUB_ACTIONS") do
+  config :react_chatroom, ReactChatroom.Repo,
+    username: "postgres",
+    password: "postgres"
+end
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :react_chatroom, ReactChatroomWeb.Endpoint,
